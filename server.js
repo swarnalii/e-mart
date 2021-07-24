eb15ab877096368d55f6eb9a23628961fe1608e1
Swarnali Mondal <itsswarnali@gmail.com>
2021-07-24 13:37:05 +0530

    ui changed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'ChatCord Bot';
+const botName = 'Bot';
 
 // Run when client connects
 io.on('connection', socket => {
@@ -27,7 +27,7 @@ io.on('connection', socket => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+    socket.emit('message', formatMessage(botName, 'Welcome to E-mart chat Room!'));
 
     // Broadcast when a user connects
     socket.broadcast
--- utils/messages.js
+++ utils/messages.js
@@ -4,7 +4,7 @@ function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().format('h:mm a')
+    time: moment().format(' h:mm a')
   };
 }
 
